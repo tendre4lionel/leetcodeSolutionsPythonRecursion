ac63cac93c682752242b9fc83ea956db81e6a97b
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 14:29:43 -0400

    An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
    Given an integer n, return true if n is an ugly number.

--- 263_Ugly Number.py
+++ 263_Ugly Number.py
@@ -1,1 +1,25 @@
 # 263 Ugly Number
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # Base case: n should be positive
+        if n <= 0:
+            return False
+        
+        # Base case: 1 is considered an ugly number
+        if n == 1:
+            return True
+        
+        # Recursively divide by 2, 3, or 5 if divisible
+        if n % 2 == 0:
+            return self.isUgly(n // 2)
+        if n % 3 == 0:
+            return self.isUgly(n // 3)
+        if n % 5 == 0:
+            return self.isUgly(n // 5)
+        
+        # If n is not divisible by 2, 3, or 5, it's not an ugly number
+        return False
