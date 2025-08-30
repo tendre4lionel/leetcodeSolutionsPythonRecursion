c4857edc34c93427073d1ed335212170a7b55bf3
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 08:57:48 -0400

    Given the root of a complete binary tree, return the number of the nodes in the tree.
    According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
    
    Design an algorithm that runs in less than O(n) time complexity.

--- 222_Count Complete Tree Nodes.py
+++ 222_Count Complete Tree Nodes.py
@@ -1,1 +1,38 @@
 # 222 Count Complete Tree Nodes
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def countNodes(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        # Base case: empty tree
+        if not root:
+            return 0
+        
+        # Helper to compute height recursively
+        def leftHeight(node):
+            if not node:
+                return 0
+            return 1 + leftHeight(node.left)
+        
+        def rightHeight(node):
+            if not node:
+                return 0
+            return 1 + rightHeight(node.right)
+        
+        hl = leftHeight(root)
+        hr = rightHeight(root)
+        
+        # If perfect tree
+        if hl == hr:
+            return 2 ** hl - 1
+        
+        # Otherwise, recursively count left and right subtrees
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
