e23962c71a2aae3fa382400cf45bdb4a59a79af1
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 11:05:50 -0400

    Given an integer n, return true if it is a power of two. Otherwise, return false.
    An integer n is a power of two, if there exists an integer x such that n == 2x.

--- 231_Power of Two.py
+++ 231_Power of Two.py
@@ -1,1 +1,29 @@
 # 231 Power of Two
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        Determine recursively if a number is a power of two.
+
+        :type n: int
+        :rtype: bool
+        """
+
+        # Base case 1: 1 is 2^0 → return True
+        if n == 1:
+            return True
+
+        # Base case 2: n <= 0 or odd numbers (except 1) are not powers of two
+        if n <= 0 or n % 2 != 0:
+            return False
+
+        # Recursive case: divide n by 2 and check recursively
+        return self.isPowerOfTwo(n // 2)
+
+
+# ---------------- Example Usage ----------------
+sol = Solution()
+print(sol.isPowerOfTwo(1))   # True, 2^0
+print(sol.isPowerOfTwo(16))  # True, 2^4
+print(sol.isPowerOfTwo(218)) # False
+print(sol.isPowerOfTwo(1024))# True, 2^10
+print(sol.isPowerOfTwo(0))   # False
