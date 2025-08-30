a959c82f2e796ac1527bc093c3d453841157b2e4
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 08:42:57 -0400

    Reverse bits of a given 32 bits signed integer.

--- 190_Reverse Bits.py
+++ 190_Reverse Bits.py
@@ -1,1 +1,21 @@
-# 190 Reverse Bits
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        # Recursive helper with accumulator
+        def helper(n, i, acc):
+            # Base case: processed 32 bits
+            if i == 32:
+                return acc
+
+            # Shift accumulator left and add least significant bit of n
+            acc = (acc << 1) | (n & 1)
+
+            # Recurse on the remaining bits
+            return helper(n >> 1, i + 1, acc)
+        
+        # Start recursion with index 0 and accumulator 0
+        return helper(n, 0, 0)
