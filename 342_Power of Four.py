83dd936e4b5f0bcd0e99a5e79b64b28188e41ea6
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 18:45:16 -0400

    Given an integer n, return true if it is a power of four. Otherwise, return false.
    An integer n is a power of four, if there exists an integer x such that n == 4x.

--- 342_Power of Four.py
+++ 342_Power of Four.py
@@ -1,1 +1,23 @@
 # 342 Power of Four
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        Check recursively if n is a power of four.
+        
+        :type n: int
+        :rtype: bool
+        """
+        # Base case: 4^0 = 1
+        if n == 1:
+            return True
+        
+        # Base case: n <= 0 cannot be power of 4
+        if n <= 0:
+            return False
+        
+        # If divisible by 4, check recursively n // 4
+        if n % 4 == 0:
+            return self.isPowerOfFour(n // 4)
+        else:
+            # Not divisible by 4 → cannot be power of 4
+            return False
