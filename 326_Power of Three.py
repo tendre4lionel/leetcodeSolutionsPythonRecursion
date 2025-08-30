79fe6fe56ba79c6a665880f2e832b7451c1a5d24
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 16:51:40 -0400

    Given an integer n, return true if it is a power of three. Otherwise, return false.
    An integer n is a power of three, if there exists an integer x such that n == 3x.

--- 326_Power of Three.py
+++ 326_Power of Three.py
@@ -1,1 +1,23 @@
 # 326 Power of Three
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        Check if n is a power of three using recursion.
+        
+        :type n: int
+        :rtype: bool
+        """
+        # Base case: 1 is 3^0
+        if n == 1:
+            return True
+        
+        # Base case: 0 or negative numbers cannot be powers of 3
+        if n <= 0:
+            return False
+        
+        # If n is divisible by 3, check recursively for n / 3
+        if n % 3 == 0:
+            return self.isPowerOfThree(n // 3)
+        else:
+            # If not divisible by 3, it cannot be a power of 3
+            return False
