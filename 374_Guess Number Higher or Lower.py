1969068727b26b36cb885b3730ac27253c4f08f4
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 19:49:42 -0400

    We are playing the Guess Game. The game is as follows:
    I pick a number from 1 to n. You have to guess which number I picked (the number I picked stays the same throughout the game).
    
    Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
    
    You call a pre-defined API int guess(int num), which returns three possible results:
    
    -1: Your guess is higher than the number I picked (i.e. num > pick).
    1: Your guess is lower than the number I picked (i.e. num < pick).
    0: your guess is equal to the number I picked (i.e. num == pick).
    Return the number that I picked.

--- 374_Guess Number Higher or Lower.py
+++ 374_Guess Number Higher or Lower.py
@@ -1,1 +1,39 @@
 # 374 Guess Number Higher or Lower
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        # Recursive helper function using binary search
+        def helper(low, high):
+            # Base case: low exceeds high (should not happen normally)
+            if low > high:
+                return -1  # safety
+
+            # Pick the middle number
+            mid = (low + high) // 2
+
+            # Use the guess API
+            res = guess(mid)
+
+            if res == 0:
+                # Found the picked number
+                return mid
+            elif res == 1:
+                # Pick is higher than mid → search upper half
+                return helper(mid + 1, high)
+            else:
+                # Pick is lower than mid → search lower half
+                return helper(low, mid - 1)
+
+        # Start recursion with full range [1, n]
+        return helper(1, n)
