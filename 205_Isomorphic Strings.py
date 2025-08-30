80a1ee6dfceb49b5776d7ddb62d578872c86666e
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 08:55:13 -0400

    Given two strings s and t, determine if they are isomorphic.
    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
    
    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.

--- 205_Isomorphic Strings.py
+++ 205_Isomorphic Strings.py
@@ -1,1 +1,28 @@
 # 205 Isomorphic Strings
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        def helper(i):
+            # Base case: reached the end of strings
+            if i == len(s):
+                return True
+            
+            # Compare first occurrence indices
+            if s.index(s[i]) != t.index(t[i]):
+                return False
+            
+            # Recurse to next character
+            return helper(i + 1)
+        
+        return helper(0)
+
+
+"""
+def isIsomorphic(s, t):
+    return [s.index(c) for c in s] == [t.index(c) for c in t]
+
+"""
