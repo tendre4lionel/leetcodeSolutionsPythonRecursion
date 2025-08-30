1ba23ad9e90965fc2d14bbd32d4b9f6b8f97bda0
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 08:00:48 -0400

    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
    
    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.

--- 88_Merge Sorted Array.py
+++ 88_Merge Sorted Array.py
@@ -1,1 +1,52 @@
 # 88 Merge Sorted Array
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        Merge two sorted arrays nums1 and nums2 into nums1 in-place.
+
+        Approach (Recursive):
+        --------------------
+        - We will fill nums1 from the **end** to avoid overwriting elements.
+        - Keep three pointers:
+            i = last valid element in nums1 (m-1)
+            j = last element in nums2 (n-1)
+            k = last position in nums1 (m+n-1)
+        - At each step:
+            1. Compare nums1[i] and nums2[j]
+            2. Place the larger one at nums1[k]
+            3. Recurse by moving the corresponding pointer(s)
+        - Base case: if nums2 is exhausted (j < 0), stop
+                     if nums1 is exhausted (i < 0), copy remaining nums2
+        """
+
+        def helper(i, j, k):
+            # Base case: if nums2 is exhausted, we are done
+            if j < 0:
+                return
+
+            # Base case: if nums1 is exhausted, copy remaining nums2 elements
+            if i < 0:
+                nums1[:j+1] = nums2[:j+1]
+                return
+
+            # Recursive case: compare last elements of current slices
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                helper(i-1, j, k-1)
+            else:
+                nums1[k] = nums2[j]
+                helper(i, j-1, k-1)
+
+        # Start recursion from the last valid elements
+        helper(m-1, n-1, m+n-1)
+
+
+# ---------------- Example Usage ----------------
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+sol = Solution()
+sol.merge(nums1, m, nums2, n)
+print(nums1)  # Output: [1,2,2,3,5,6]
