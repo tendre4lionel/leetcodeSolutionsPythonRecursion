bb3663933bf7c5972ce1f04312d5f8ca00bbdfab
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 18:57:40 -0400

    Write a function that reverses a string. The input string is given as an array of characters s.
    You must do this by modifying the input array in-place with O(1) extra memory.

--- 344_Reverse String.py
+++ 344_Reverse String.py
@@ -1,1 +1,27 @@
 # 344 Reverse String
+class Solution(object):
+    def reverseString(self, s):
+        """
+        Reverse the list of characters in-place using recursion.
+        
+        :type s: List[str]
+        :rtype: None
+        """
+        # Recursive helper function
+        def helper(left, right):
+            # Base case: pointers crossed → stop recursion
+            if left >= right:
+                return
+            
+            # Swap characters at left and right
+            s[left], s[right] = s[right], s[left]
+            
+            # Recurse for the next pair
+            helper(left + 1, right - 1)
+        
+        # Start recursion from the ends of the array
+        helper(0, len(s) - 1)
+
+
+# Oneliner s.reverse()
+# In Place s[:] = s[::-1]
