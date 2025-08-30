3840f649b8b2b35cc2b969d36964e92dfdbc1989
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 14:52:07 -0400

    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
    
    You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.

--- 278_First Bad Version.py
+++ 278_First Bad Version.py
@@ -1,1 +1,31 @@
 # 278 First Bad Version
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Recursive binary search helper
+        def search(left, right):
+            # Base case: when left == right, we've found the first bad version
+            if left == right:
+                return left
+            
+            mid = left + (right - left) // 2
+            
+            if isBadVersion(mid):
+                # First bad version is in the left half (including mid)
+                return search(left, mid)
+            else:
+                # First bad version is in the right half (excluding mid)
+                return search(mid + 1, right)
+        
+        return search(1, n)
