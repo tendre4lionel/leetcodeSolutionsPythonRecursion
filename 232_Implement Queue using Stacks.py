f5ce91018f9eef6b0a81662c3304907f8fdf4bae
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 14:35:04 -0400

    class MyQueue(object):     def __init__(self):         # Initialize two stacks         self.in_stack = []         self.out_stack = []
        def push(self, x):
            """
            Push element x to the back of the queue.
            """
            # Always push to in_stack
            self.in_stack.append(x)
    
        def pop(self):
            """
            Removes the element from the front of the queue and returns it.
            """
            # If out_stack is empty, move all elements from in_stack to out_stack
            if not self.out_stack:
                while self.in_stack:
                    self.out_stack.append(self.in_stack.pop())
            # Pop from out_stack (this gives us the front of the queue)
            return self.out_stack.pop()
    
        def peek(self):
            """
            Get the front element.
            """
            # If out_stack is empty, move all elements from in_stack to out_stack
            if not self.out_stack:
                while self.in_stack:
                    self.out_stack.append(self.in_stack.pop())
            # Peek from out_stack (this gives us the front of the queue)
            return self.out_stack[-1]
    
        def empty(self):
            """
            Return whether the queue is empty.
            """
            # Queue is empty if both in_stack and out_stack are empty
            return not self.in_stack and not self.out_stack

--- 232_Implement Queue using Stacks.py
+++ 232_Implement Queue using Stacks.py
@@ -1,51 +1,45 @@
 # 232 Implement Queue using Stacks
 class MyQueue(object):
     def __init__(self):
-        """
-        Initialize your data structure here.
-        We use two stacks:
-        1. stack_in: for push operations (new elements enter here)
-        2. stack_out: for pop/peek operations (elements to be dequeued)
-        """
-        self.stack_in = []   # Stack to store new elements
-        self.stack_out = []  # Stack to store elements in correct dequeue order
+        # Initialize two stacks
+        self.in_stack = []
+        self.out_stack = []
 
     def push(self, x):
         """
-        Push element x to the back of queue.
-        Simply push it to stack_in.
+        Push element x to the back of the queue.
         """
-        self.stack_in.append(x)
+        # Always push to in_stack
+        self.in_stack.append(x)
 
     def pop(self):
         """
-        Removes the element from in front of queue and returns it.
-        If stack_out is empty, pour all elements from stack_in to stack_out
-        to reverse the order (so the oldest element is on top of stack_out).
+        Removes the element from the front of the queue and returns it.
         """
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
-        # Pop from stack_out which is the front of the queue
-        return self.stack_out.pop()
+        # If out_stack is empty, move all elements from in_stack to out_stack
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        # Pop from out_stack (this gives us the front of the queue)
+        return self.out_stack.pop()
 
     def peek(self):
         """
         Get the front element.
-        If stack_out is empty, pour all elements from stack_in to stack_out.
-        Then peek at the top of stack_out.
         """
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
-        return self.stack_out[-1]  # Peek top element without popping
+        # If out_stack is empty, move all elements from in_stack to out_stack
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        # Peek from out_stack (this gives us the front of the queue)
+        return self.out_stack[-1]
 
     def empty(self):
         """
-        Returns whether the queue is empty.
-        Queue is empty if both stacks are empty.
+        Return whether the queue is empty.
         """
-        return not self.stack_in and not self.stack_out
+        # Queue is empty if both in_stack and out_stack are empty
+        return not self.in_stack and not self.out_stack
 
 
 # ---------------- Example Usage ----------------
