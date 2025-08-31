cf9a8f111fa487513e0f96a3f15c1babd996c013
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 22:09:33 -0400

    Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).

--- 392_Is Subsequence.py
+++ 392_Is Subsequence.py
@@ -1,1 +1,31 @@
-# 392 Is Subsequence
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        # Recursive helper function
+        def check(i, j):
+            """
+            i: current index in s
+            j: current index in t
+            """
+            # Base case 1: All characters in s matched
+            if i == len(s):
+                return True
+
+            # Base case 2: Reached end of t without matching all of s
+            if j == len(t):
+                return False
+
+            # If current characters match, move both pointers
+            if s[i] == t[j]:
+                return check(i + 1, j + 1)
+            else:
+                # Else, move pointer in t to find a match
+                return check(i, j + 1)
+
+        # Start recursion from index 0 for both strings
+        return check(0, 0)
