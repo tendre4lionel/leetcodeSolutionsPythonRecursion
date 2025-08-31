d1c89240f3f87ac0f74ebf5f5b65ace551d4e5c1
tendre4lionel <tendre4lionel@gmail.com>
2025-08-31 18:41:21 -0400

    Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
    Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.

--- 455_Assign Cookies.py
+++ 455_Assign Cookies.py
@@ -1,1 +1,49 @@
 # 455 Assign Cookies
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+
+        def helper(i, j):
+            # i -> index in g (children), j -> index in s (cookies)
+            if i < 0 or j < 0:
+                return 0
+
+            if s[j] >= g[i]:
+                # assign this cookie to this child
+                return 1 + helper(i - 1, j - 1)
+            else:
+                # cookie too small, skip it
+                return helper(i, j - 1)
+
+        return helper(len(g) - 1, len(s) - 1)
+
+
+
+
+"""
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+
+        i = 0  # pointer for children
+        j = 0  # pointer for cookies
+
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:   # cookie satisfies this child
+                i += 1         # move to next child
+            j += 1             # always move to next cookie
+
+        return i
+"""
