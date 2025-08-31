30109c974ea5492a0eeff357131f7bdfba56bc67
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 22:32:16 -0400

    A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
    For example, the below binary watch reads "4:51".
    
    Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM), return all possible times the watch could represent. You may return the answer in any order.
    
    The hour must not contain a leading zero.
    
    For example, "01:00" is not valid. It should be "1:00".
    The minute must consist of two digits and may contain a leading zero.
    
    For example, "10:2" is not valid. It should be "10:02".
    
    Example 1:
    
    Input: turnedOn = 1
    Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
    Example 2:
    
    Input: turnedOn = 9
    Output: []
    
    Constraints:
    
    0 <= turnedOn <= 10

--- 401_Binary Watch.py
+++ 401_Binary Watch.py
@@ -1,1 +1,70 @@
 # 401 Binary Watch
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        """
+        :type turnedOn: int
+        :rtype: List[str]
+        """
+        result = []
+
+        # Recursive helper function to turn on LEDs
+        def dfs(pos, leds_left, hour, minute):
+            """
+            pos: current LED position (0-9)
+            leds_left: number of LEDs left to turn on
+            hour: current hour value
+            minute: current minute value
+            """
+            # Base case: all LEDs turned on
+            if leds_left == 0:
+                if 0 <= hour <= 11 and 0 <= minute <= 59:
+                    # Format minute with leading zero
+                    result.append("{}:{:02d}".format(hour, minute))
+                return
+
+            # If all positions processed, stop
+            if pos == 10:
+                return
+
+            # Determine if current LED is for hour or minute
+            if pos < 4:
+                # Hour LED, value = 2^pos
+                new_hour = hour + (1 << pos)
+                if new_hour <= 11:
+                    dfs(pos + 1, leds_left - 1, new_hour, minute)
+            else:
+                # Minute LED, value = 2^(pos-4)
+                new_minute = minute + (1 << (pos - 4))
+                if new_minute <= 59:
+                    dfs(pos + 1, leds_left - 1, hour, new_minute)
+
+            # Also explore skipping this LED
+            dfs(pos + 1, leds_left, hour, minute)
+
+        # Start recursion from first LED
+        dfs(0, turnedOn, 0, 0)
+
+        return result
+
+
+"""
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        """
+        :type turnedOn: int
+        :rtype: List[str]
+        """
+        result = []
+
+        # Iterate over all possible hours (0-11)
+        for hour in range(12):
+            # Iterate over all possible minutes (0-59)
+            for minute in range(60):
+                # Count total number of '1's in binary representation
+                if (bin(hour) + bin(minute)).count('1') == turnedOn:
+                    # Format minute with leading zero using str.format()
+                    result.append("{}:{:02d}".format(hour, minute))
+
+        return result
+
+"""
