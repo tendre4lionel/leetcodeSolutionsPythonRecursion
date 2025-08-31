3f717cad2f7d67b3ad4dc4c99d43dba3bfcc762c
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 23:16:31 -0400

    Given an integer n, return a string array answer (1-indexed) where:
    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
    answer[i] == "Fizz" if i is divisible by 3.
    answer[i] == "Buzz" if i is divisible by 5.
    answer[i] == i (as a string) if none of the above conditions are true.

--- 412_Fizz Buzz.py
+++ 412_Fizz Buzz.py
@@ -1,1 +1,32 @@
 # 412 Fizz Buzz
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+
+        # Recursive helper function
+        def helper(i):
+            # Base case: all numbers processed
+            if i > n:
+                return
+
+            # Determine Fizz/Buzz/FizzBuzz
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+
+            # Recurse for next number
+            helper(i + 1)
+
+        # Start recursion from 1
+        helper(1)
+
+        return result
