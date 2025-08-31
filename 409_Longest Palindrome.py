eb940c4a864e51be3152f671aa0ad498a2f6f6dc
tendre4lionel <tendre4lionel@gmail.com>
2025-08-30 23:15:50 -0400

    Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
    Letters are case sensitive, for example, "Aa" is not considered a palindrome.

--- 409_Longest Palindrome.py
+++ 409_Longest Palindrome.py
@@ -1,1 +1,24 @@
 # 409 Longest Palindrome
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_set = set()
+        length = 0
+
+        for char in s:
+            if char in char_set:
+                # Found a pair → remove from set and add 2 to length
+                char_set.remove(char)
+                length += 2
+            else:
+                # First occurrence → add to set
+                char_set.add(char)
+
+        # If any characters remain in set, one can go in the center
+        if char_set:
+            length += 1
+
+        return length
